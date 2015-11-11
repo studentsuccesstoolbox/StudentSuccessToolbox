f2134aa87a688c0b3dd01f8984d81eca12e883f8
Anthony Leonard <leonard.ant_at_gmail.com>
2015-11-11 16:12:32 +0000

    make last bit of the flow, before starting on the 6 interim flows and data gathering tasks!

--- tool8/app/app.js
+++ tool8/app/app.js
@@ -72,6 +72,14 @@ sstTool8App.config(function($routeProvider) {
     		templateUrl: 'app/views/templates/elements-of-an-online-orientation.html',
                 controller: 'defaultController'
     	})
+        .when('/online-orientation-summary/', {
+    		templateUrl: 'app/views/templates/online-orientation-summary.html',
+                controller: 'defaultController'
+    	})
+        .when('/review-your-online-orientation-plan/', {
+    		templateUrl: 'app/views/templates/review-your-online-orientation-plan.html',
+                controller: 'defaultController'
+    	})
 	.when('/resources/', {
             templateUrl: 'app/views/templates/resources.html',
             controller: 'defaultController'
