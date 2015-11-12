416eff1eca32f0a26ad3f6838314d35d763f93d8
Anthony Leonard <leonard.ant_at_gmail.com>
2015-11-12 16:35:37 +0000

    add text from powerpoint and plumb in questionnaire - partial!

--- tool8/app/data/tool8Questionnaire.js
+++ tool8/app/data/tool8Questionnaire.js
@@ -5,147 +5,155 @@
  */
 
 tool8Questionnaire = {
-    'your-online-orientation' : {
-            'label' : 'your online orientation'
-            ,'description' : 'your online orientation'
-            ,'quotes' : [
-            ]
-            ,'questions' :[{
-                'toolCategory':'Location',
-                'question':'Will you build your online orientation in your institutional Virtual Learning Environment (VLE), e.g. Moodle or Blackboard?'
-                ,'response' : ''
-                ,selected : ''
-                ,options: [{
-                  'label': 'YES'
-                  ,'value': 'yes'
-                  ,'answer': ''
-                  ,'weight': 1
-                },{
-                  'label': 'NO'
-                  ,'value': 'no'
-                  ,'answer': ''
-                  ,'weight': 0
-                }]
+    'your-online-orientation': {
+        'label': 'your online orientation'
+        , 'description': 'your online orientation'
+        , 'quotes': [
+        ]
+        , 'questions': [{
+                'toolCategory': 'Location',
+                'question': 'Will you build your online orientation in your institutional Virtual Learning Environment (VLE), e.g. Moodle or Blackboard?',
+                'questionSmall': 'Virtual Learning Environment (VLE)'
+                , 'response': ''
+                , selected: ''
+                , options: [{
+                        'label': 'YES'
+                        , 'value': 'yes'
+                        , 'answer': ''
+                        , 'weight': 1
+                    }, {
+                        'label': 'NO'
+                        , 'value': 'no'
+                        , 'answer': ''
+                        , 'weight': 0
+                    }]
             },
             {
-                'toolCategory':'',
-                'question':'Will you build your online orientation within your institutional website?'
-                ,'response' : ''
-                ,selected : ''
-                ,options: [{
-                  'label': 'YES'
-                  ,'value': 'yes'
-                  ,'answer': ''
-                  ,'weight': 1
-                },{
-                  'label': 'NO'
-                  ,'value': 'no'
-                  ,'answer': ''
-                  ,'weight': 0
-                }]
+                'toolCategory': '',
+                'question': 'Will you build your online orientation within your institutional website?',
+                'questionSmall': 'Website'
+                , 'response': ''
+                , selected: ''
+                , options: [{
+                        'label': 'YES'
+                        , 'value': 'yes'
+                        , 'answer': ''
+                        , 'weight': 1
+                    }, {
+                        'label': 'NO'
+                        , 'value': 'no'
+                        , 'answer': ''
+                        , 'weight': 0
+                    }]
             }
-            ,{
-                'toolCategory':'Technology',
-                'question':'Do you have access to equipment and software that you can use to produce video content for your online orientation?'
-                ,'response' : ''
-                ,selected : ''
-                ,options: [{
-                  'label': 'YES'
-                  ,'value': 'yes'
-                  ,'answer': ''
-                  ,'weight': 1
-                },{
-                  'label': 'NO'
-                  ,'value': 'no'
-                  ,'answer': ''
-                  ,'weight': 0
-                }]
+            , {
+                'toolCategory': 'Technology',
+                'question': 'Do you have access to equipment and software that you can use to produce video content for your online orientation?',
+                'questionSmall': 'Video'
+                , 'response': ''
+                , selected: ''
+                , options: [{
+                        'label': 'YES'
+                        , 'value': 'yes'
+                        , 'answer': ''
+                        , 'weight': 1
+                    }, {
+                        'label': 'NO'
+                        , 'value': 'no'
+                        , 'answer': ''
+                        , 'weight': 0
+                    }]
             }
-            ,{
-                'toolCategory':'',
-                'question':'Do you have access to equipment and software that you can use to produce audio content for your online orientation?'
-                ,'response' : ''
-                ,selected : ''
-                ,options: [{
-                  'label': 'YES'
-                  ,'value': 'yes'
-                  ,'answer': ''
-                  ,'weight': 1
-                },{
-                  'label': 'NO'
-                  ,'value': 'no'
-                  ,'answer': ''
-                  ,'weight': 0
-                }]
+            , {
+                'toolCategory': '',
+                'question': 'Do you have access to equipment and software that you can use to produce audio content for your online orientation?',
+                'questionSmall': 'Audio'
+                , 'response': ''
+                , selected: ''
+                , options: [{
+                        'label': 'YES'
+                        , 'value': 'yes'
+                        , 'answer': ''
+                        , 'weight': 1
+                    }, {
+                        'label': 'NO'
+                        , 'value': 'no'
+                        , 'answer': ''
+                        , 'weight': 0
+                    }]
             }
-            ,{
-                'toolCategory':'',
-                'question':'Do you have access to e-learning platform software, e.g. Articulate, that you can use to produce content for your online orientation?'
-                ,'response' : ''
-                ,selected : ''
-                ,options: [{
-                  'label': 'YES'
-                  ,'value': 'yes'
-                  ,'answer': ''
-                  ,'weight': 1
-                },{
-                  'label': 'NO'
-                  ,'value': 'no'
-                  ,'answer': ''
-                  ,'weight': 0
-                }]
+            , {
+                'toolCategory': '',
+                'question': 'Do you have access to e-learning platform software, e.g. Articulate, that you can use to produce content for your online orientation?',
+                'questionSmall': 'E-learning platform'
+                , 'response': ''
+                , selected: ''
+                , options: [{
+                        'label': 'YES'
+                        , 'value': 'yes'
+                        , 'answer': ''
+                        , 'weight': 1
+                    }, {
+                        'label': 'NO'
+                        , 'value': 'no'
+                        , 'answer': ''
+                        , 'weight': 0
+                    }]
             }
-            ,{
-                'toolCategory':'',
-                'question':'Do you have access to presentation tools, e.g. Powerpoint or Prezi, that you can use to produce content for your online orientation?'
-                ,'response' : ''
-                ,selected : ''
-                ,options: [{
-                  'label': 'YES'
-                  ,'value': 'yes'
-                  ,'answer': ''
-                  ,'weight': 1
-                },{
-                  'label': 'NO'
-                  ,'value': 'no'
-                  ,'answer': ''
-                  ,'weight': 0
-                }]
+            , {
+                'toolCategory': '',
+                'question': 'Do you have access to presentation tools, e.g. Powerpoint or Prezi, that you can use to produce content for your online orientation?',
+                'questionSmall': 'Presentation Tools'
+                , 'response': ''
+                , selected: ''
+                , options: [{
+                        'label': 'YES'
+                        , 'value': 'yes'
+                        , 'answer': ''
+                        , 'weight': 1
+                    }, {
+                        'label': 'NO'
+                        , 'value': 'no'
+                        , 'answer': ''
+                        , 'weight': 0
+                    }]
             }
-            ,{
-                'toolCategory':'Existing Resources',
-                'question':'Can you locate open educational resources that you can use in your online orientation?'
-                ,'response' : ''
-                ,selected : ''
-                ,options: [{
-                  'label': 'YES'
-                  ,'value': 'yes'
-                  ,'answer': ''
-                  ,'weight': 1
-                },{
-                  'label': 'NO'
-                  ,'value': 'no'
-                  ,'answer': ''
-                  ,'weight': 0
-                }]
+            , {
+                'toolCategory': 'Existing Resources',
+                'question': 'Can you locate open educational resources that you can use in your online orientation?',
+                'questionSmall': 'Open Educational Resources'
+                , 'response': ''
+                , selected: ''
+                , options: [{
+                        'label': 'YES'
+                        , 'value': 'yes'
+                        , 'answer': ''
+                        , 'weight': 1
+                    }, {
+                        'label': 'NO'
+                        , 'value': 'no'
+                        , 'answer': ''
+                        , 'weight': 0
+                    }]
             }
-            ,{
-                'toolCategory':'',
-                'question':'Are there existing institutional resources that you can use in you online orientation?'
-                ,'response' : ''
-                ,selected : ''
-                ,options: [{
-                  'label': 'YES'
-                  ,'value': 'yes'
-                  ,'answer': ''
-                  ,'weight': 1
-                },{
-                  'label': 'NO'
-                  ,'value': 'no'
-                  ,'answer': ''
-                  ,'weight': 0
-                }]
+            , {
+                'toolCategory': '',
+                'question': 'Are there existing institutional resources that you can use in you online orientation?',
+                'questionSmall': 'Existing Institutional Resources'
+                , 'response': ''
+                , selected: ''
+                , options: [{
+                        'label': 'YES'
+                        , 'value': 'yes'
+                        , 'answer': ''
+                        , 'weight': 1
+                    }, {
+                        'label': 'NO'
+                        , 'value': 'no'
+                        , 'answer': ''
+                        , 'weight': 0
+                    }]
             }
         ]
-    }  
+    }
 };
